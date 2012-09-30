900ee2b56f86e93b57d330449d2cc88b2bf7d6da
Graham Gilbert <graham@grahamgilbert.com>
2012-09-30 17:12:33 +0100

    Fixed some tab spacing

--- macnamer/urls.py
+++ macnamer/urls.py
@@ -8,9 +8,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'macnamer.views.home', name='home'),
     # url(r'^macnamer/', include('macnamer.foo.urls')),
-	url(r'^login/$', 'django.contrib.auth.views.login'),
-	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
-    url(r'^app/', include('namer.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+   	url(r'^app/', include('namer.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
