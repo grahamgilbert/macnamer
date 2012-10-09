d51c5bb7f9cf441f8cbf21cf821c24c578f69bb8
Graham Gilbert <graham@grahamgilbert.com>
2012-10-09 12:30:20 +0100

    Cleaned up the models - don't need to two models

--- namer/models.py
+++ namer/models.py
@@ -5,25 +5,13 @@ from django.db.models.signals import post_save
 # Create your models here.
 class ComputerGroup(models.Model):
     name = models.CharField(max_length=200)
+    prefix = models.CharField(max_length=200, verbose_name="Computer Name Prefix",blank=True,null=True)
+    domain = models.CharField(max_length=200, verbose_name="Computer Domain",blank=True,null=True)
     def __unicode__(self):
         if self.name:
             return self.name
         else:
             return self.id
- 
-    
-class Prefix(models.Model):
-    prefix = models.CharField(max_length=200, verbose_name="Computer Name Prefix",blank=True,null=True)
-    domain = models.CharField(max_length=200, verbose_name="Computer Domain",blank=True,null=True)
-    computer_group = models.ForeignKey(ComputerGroup)
-    def __unicode__(self):
-        return self.prefix
-        
-def create_prefix(sender, instance, created, **kwargs):
-    if created:
-        Prefix.objects.create(ComputerGroup=instance)
-#only oneprefix per group for now, so this will handle the creating of eachone
-post_save.connect(create_prefix, sender=ComputerGroup)
 
 class Computer(models.Model):
     prefix = models.ForeignKey(Prefix)
