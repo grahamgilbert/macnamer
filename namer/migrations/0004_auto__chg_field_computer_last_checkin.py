8d2205696390a8df99958706b885051a4b85fd14
Graham - Linode <graham@grahamgilbert.com>
2012-10-10 14:35:06 +0100

    schema migrations

--- namer/migrations/0004_auto__chg_field_computer_last_checkin.py
+++ namer/migrations/0004_auto__chg_field_computer_last_checkin.py
@@ -8,29 +8,21 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding field 'Computer.last_checkin'
-        db.add_column('namer_computer', 'last_checkin',
-                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2012, 10, 10, 0, 0)),
-                      keep_default=False)
 
-        # Adding unique constraint on 'Computer', fields ['serial']
-        db.create_unique('namer_computer', ['serial'])
-
+        # Changing field 'Computer.last_checkin'
+        db.alter_column('namer_computer', 'last_checkin', self.gf('django.db.models.fields.DateTimeField')(null=True))
 
     def backwards(self, orm):
-        # Removing unique constraint on 'Computer', fields ['serial']
-        db.delete_unique('namer_computer', ['serial'])
 
-        # Deleting field 'Computer.last_checkin'
-        db.delete_column('namer_computer', 'last_checkin')
-
+        # User chose to not deal with backwards NULL issues for 'Computer.last_checkin'
+        raise RuntimeError("Cannot reverse this migration. 'Computer.last_checkin' and its values cannot be restored.")
 
     models = {
         'namer.computer': {
             'Meta': {'object_name': 'Computer'},
             'computergroup': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['namer.ComputerGroup']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_checkin': ('django.db.models.fields.DateTimeField', [], {}),
+            'last_checkin': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'serial': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'})
         },
