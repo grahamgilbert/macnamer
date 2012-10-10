1830094e378d577f7a9c189ae45dc3dfb249af1c
Graham Gilbert <graham@grahamgilbert.com>
2012-10-10 14:49:04 +0100

    Still trying to work on the sorting

--- namer/views.py
+++ namer/views.py
@@ -89,7 +89,7 @@ def new_computer(request, group_id):
         else:
             initial_name = ""
         form = ComputerForm(initial={'name': initial_name})
-    c = {'form': form, 'group':group, }
+    c = {'form': form, 'group':group, 'maximum_name':maximum_name, }
     return render_to_response('forms/new_computer.html', c, context_instance=RequestContext(request))
 
 #edit computer
