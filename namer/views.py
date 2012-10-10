73ad8e7b761c59c7b343b77e58629ef7029d74c0
Graham Gilbert <graham@grahamgilbert.com>
2012-10-10 06:48:41 +0100

    Fixing bug with editing computers

--- namer/views.py
+++ namer/views.py
@@ -94,7 +94,7 @@ def edit_computer(request, computer_id):
             return redirect('namer.views.show_group', computer.computergroup.id)
     else:
         form = ComputerForm(instance=computer)
-    c = {'form': form, 'group':computer.computergroup, }
+    c = {'form': form, 'group':computer.computergroup, 'computer':computer, }
     return render_to_response('forms/edit_computer.html', c, context_instance=RequestContext(request))
 #show computer group
 @login_required
